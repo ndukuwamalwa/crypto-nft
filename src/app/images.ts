6662fe42f953ffbaceead402867d0c2c24823b8e
Justin Wamalwa <justinwamalwa3@gmail.com>
2023-02-17 16:10:34 +0300

    Landing page

---
+++ src/app/images.ts
@@ -0,0 +1,179 @@
+import { IPhoto } from "./models";
+
+export const APP_PROTOS: Array<IPhoto> = [
+    {
+        url: '1.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '2.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '3.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '4.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '5.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '6.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '7.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '8.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '9.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '10.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '11.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '12.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '13.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '14.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '15.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '16.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '17.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '18.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '19.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '20.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '21.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '22.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '23.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '24.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '25.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '26.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '27.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '28.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '29.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '30.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '31.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '32.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '33.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '34.avif',
+        creator: '@john.doe',
+        amount: 10
+    },
+    {
+        url: '35.avif',
+        creator: '@john.doe',
+        amount: 10
+    }
+]
---
+++ src/app/models.ts
@@ -0,0 +1,5 @@
+export interface IPhoto {
+    url: string;
+    creator: string;
+    amount: number;
+}
