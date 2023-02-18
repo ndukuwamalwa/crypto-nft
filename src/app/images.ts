1fb41905d25cc109e205d5c1551e3bede313acf7
Justin Wamalwa <justinwamalwa3@gmail.com>
2023-02-18 11:05:15 +0300

    Removed heavy images

--- src/app/images.ts
+++ src/app/images.ts
@@ -14,22 +14,10 @@ export const APP_PROTOS: Array<IPhoto> = [
         name: 'infinity #11'
     },
     {
-        url: '3.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #12'
-    },
-    {
         url: '4.avif',
         creator: '@john.doe',
         amount: 10,
         name: 'infinity #13'
-    },
-    {
-        url: '5.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #14'
     },
     {
         url: '6.avif',
@@ -44,40 +32,10 @@ export const APP_PROTOS: Array<IPhoto> = [
         name: 'infinity #16'
     },
     {
-        url: '8.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #17'
-    },
-    {
-        url: '9.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #18'
-    },
-    {
-        url: '10.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #19'
-    },
-    {
-        url: '11.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #20'
-    },
-    {
         url: '12.avif',
         creator: '@john.doe',
         amount: 10,
         name: 'infinity #21'
-    },
-    {
-        url: '13.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #22'
     },
     {
         url: '14.avif',
@@ -86,129 +44,9 @@ export const APP_PROTOS: Array<IPhoto> = [
         name: 'infinity #23'
     },
     {
-        url: '15.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #24'
-    },
-    {
         url: '16.avif',
         creator: '@john.doe',
         amount: 10,
         name: 'infinity #25'
-    },
-    {
-        url: '17.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #26'
-    },
-    {
-        url: '18.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #27'
-    },
-    {
-        url: '19.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #28'
-    },
-    {
-        url: '20.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #29'
-    },
-    {
-        url: '21.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #30'
-    },
-    {
-        url: '22.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #31'
-    },
-    {
-        url: '23.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #32'
-    },
-    {
-        url: '24.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #33'
-    },
-    {
-        url: '25.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #34'
-    },
-    {
-        url: '26.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #35'
-    },
-    {
-        url: '27.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #36'
-    },
-    {
-        url: '28.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #37'
-    },
-    {
-        url: '29.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #38'
-    },
-    {
-        url: '30.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #39'
-    },
-    {
-        url: '31.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #40'
-    },
-    {
-        url: '32.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #41'
-    },
-    {
-        url: '33.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #42'
-    },
-    {
-        url: '34.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #43'
-    },
-    {
-        url: '35.avif',
-        creator: '@john.doe',
-        amount: 10,
-        name: 'infinity #44'
     }
 ]
